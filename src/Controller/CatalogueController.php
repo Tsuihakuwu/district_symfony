3aa0a7934419b55f32f8fff8486b12252b3f556b
Matthias <matthias.sergeant@gmail.com>
2023-06-09 12:22:47 +0200

    1.05 - Routes categorie et plat

--- src/Controller/CatalogueController.php
+++ src/Controller/CatalogueController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use App\Repository\CommandeRepository;
 use App\Repository\DetailRepository;
@@ -9,6 +10,7 @@ use App\Repository\PlatRepository;
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,11 +39,7 @@ class CatalogueController extends AbstractController
     public function index(): Response
     {
 
-        $utilisateurs =  $this->utilisateurRepository->findAll();
-        $commandes = $this->commandeRepository->findAll();
-        $details = $this->detailRepository->findAll();
-        $plats = $this->platRepository->findAll();
-        $categories = $this->categorieRepository->findAll();
+        $this->utilisateurRepository->findAll();
 
         //Requête pour afficher les 6 catégories les plus populaires et completer avec d'autres si la base n'est pas assez alimentée pour obtenir un total de 6
         $query = <<<SQL
@@ -67,6 +65,27 @@ class CatalogueController extends AbstractController
         $statement = $connection->executeQuery($query);
         $results = $statement->fetchAllAssociative();
 
+        // $subquery = $this->entityManager->createQuery('SELECT categorie.libelle, categorie.image, categorie.id
+        // FROM detail
+        // JOIN plat ON plat.id = detail.plat_id
+        // JOIN commande ON commande.id = detail.commande_id
+        // JOIN categorie ON plat.categorie_id = categorie.id
+        // WHERE commande.etat = 3 AND categorie.active = 1 AND plat.active = 1
+        // GROUP BY categorie.libelle
+        // ORDER BY COUNT(*) DESC');
+
+        // $subqueryresults = $subquery->getResult();
+
+        // $query = $this->entityManager->createQuery('SELECT T1.libelle, T1.image, T1.id
+        // FROM (:subquery) AS T1
+        // UNION
+        // SELECT categorie.libelle, categorie.image, categorie.id
+        // FROM categorie
+        // WHERE categorie.active = 1
+        // LIMIT 6')->setParameter('subquery', $subqueryresults);
+
+        // $results = $query->getResult();
+
         $mostpopplat = $this->commandeRepository->mostpopplat();
 
         return $this->render('catalogue/index.html.twig', [
@@ -89,6 +108,49 @@ class CatalogueController extends AbstractController
     {
         return $this->render('ml/index.html.twig', [
             'controller_name' => 'CatalogueController',
+        ]);
+    }
+
+    #[Route('/categorie', name: 'app_categorie')]
+    public function categorie(): Response
+    {
+        $utilisateurs =  $this->utilisateurRepository->findAll();
+        $categories = $this->categorieRepository->findBy(['active' => true]);
+
+        return $this->render('categorie/index.html.twig', [
+            'controller_name' => 'CatalogueController',
+            'cat' => $categories,
+        ]);
+    }
+
+    #[Route('/plat', name: 'app_plat')]
+    public function plat(): Response
+    {
+
+        $utilisateurs =  $this->utilisateurRepository->findAll();
+        $plats = $this->platRepository->findBy(['active' => true]);
+
+        return $this->render('plat/index.html.twig', [
+            'controller_name' => 'CatalogueController',
+            'plats' => $plats
+        ]);
+    }
+
+    #[Route('/plat/{categorie_id}', name: 'app_platwithcat')]
+    public function platwithcat(Request $request, Categorie $categorie_id): Response
+    {
+        // TODO:Utiliser l'id récupéré de l'objet pour créer la vue findby idcat
+        // dd($categorie_id);
+        if($request->get('categorie_id')){
+            $categorie_id = $request->get('categorie_id');
+            
+        }
+        $utilisateurs =  $this->utilisateurRepository->findAll();
+        $plats = $this->platRepository->findBy(['active' => true, 'categorie' => $categorie_id]);
+
+        return $this->render('plat/index.html.twig', [
+            'controller_name' => 'CatalogueController',
+            'plats' => $plats
         ]);
     }
 }
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +13,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $form = $this->createForm(ContactFormType::class);
+
+        // $form = $this->createForm(ContactFormType::class, null, [
+            // 'action' => $this->generateUrl('ma_route_spécifique'),
+        // ]);>
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()&&$form->isValid()){
+
+            $message = new Contact();
+            $data = $form->getData();
+            $message = $data;
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('app_accueil');
+        }
+
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
+            'form' => $form
         ]);
     }
 }
