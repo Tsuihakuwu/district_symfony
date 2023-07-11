d048d7f4b91337cc3e4bbbbcffb97845998f083a
Matthias <matthias.sergeant@gmail.com>
2023-07-11 16:53:03 +0200

    2.07 - construction mail commande

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,7 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\Commande;
+use App\Entity\Detail;
+use App\Repository\UtilisateurRepository;
+use App\Service\MailService;
 use App\Service\PanierService;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,44 +16,79 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommandeController extends AbstractController
 {
     private $panier;
+    private $utilisateurRepository;
 
-    public function __construct(PanierService $panierService)
+    public function __construct(PanierService $panierService, UtilisateurRepository $utilisateurRepository)
     {
         $this->panier = $panierService;
+        $this->utilisateurRepository = $utilisateurRepository;
     }
 
     #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager, MailService $ms): Response
     {
         $panier = $this->panier->getPanier();
-        if($panier != NULL){
+        if ($panier != NULL) {
             $total = $this->panier->getTotalPrix();
             $qtt = $this->panier->getTotalQuantity();
-        }
-        else {
+        } else {
             $total = 0;
             $qtt = "";
         }
-
-        // FIXME: Recupérer proprement un objet user pour traitement
 
         $this->denyAccessUnlessGranted('ROLE_CLIENT');
 
         $user = $this->getUser();
 
-        $user->getNom();
+        $identifiant = $this->getUser()->getUserIdentifier();
+        if ($identifiant) {
+            $info = $this->utilisateurRepository->findOneBy(["email" => $identifiant]);
+        } else {
+            $this->addFlash('error', 'Erreur utilisateur: Veuillez vous reconnecter');
+            return $this->redirectToRoute('app_accueil');
+        }
 
-        // $userdata = [;];
+        //Info utilisateurs
 
-        dd($user);
+        $adresse_client = $info->getAdresse() . " " . $info->getCp() . " " . $info->getVille();
+        $nom_client = $info->getPrenom() . " " . $info->getNom();
+        $mail_client = $info->getEmail();
+
+        //Entrée base de données        
+
+        $commande = new Commande();
+        $commande->setUtilisateur($info);
+        $now = new DateTime();
+        $commande->setDateCommande($now);
+        $commande->setTotal($total);
+        $commande->setEtat(0);        
+
+        $entityManager->persist($commande);
+        $entityManager->flush();
+
+        foreach($item as $panier){
+            $detail = new Detail();
+            $detail->setQuantite($qtt);
+        }
         
-        // TODO: Coder inscription en base de données d'une nouvelle commande et VIDER LE PANIER ?
+        
+        $detail->setPlat()
+
+
+        //Création Mail
+
+        // $myMail = 'commande@thedistrict.fr';
+        // $ms->sendMail($data->getEmail(), $myMail, $data->getObjet(), $data->getMessage());
+
 
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
             'panier' => $panier,
             'totalprix' => $total,
-            'userdata' => $userdata,
+            'adresse_client' => $adresse_client,
+            'nom_client' => $nom_client,
+            'mail_client' => $mail_client,
+
         ]);
-        }
+    }
 }
--- var/cache/dev/twig/b4/b4a367b8a3ecef208c2746b5d48172ed.php
+++ var/cache/dev/twig/b4/b4a367b8a3ecef208c2746b5d48172ed.php
@@ -88,42 +88,47 @@ class __TwigTemplate_49c6cb55856af072d44c006a74fd2962 extends Template
         // line 7
         echo "
 \t<main>
-\t\t<h1 class=\"d-flex justify-content-center mt-2\">Merci de votre commande !</h1>
+\t\t<h1 class=\"d-flex justify-content-center mt-2\">Merci de votre commande
+\t\t\t";
+        // line 10
+        echo twig_escape_filter($this->env, (isset($context["nom_client"]) || array_key_exists("nom_client", $context) ? $context["nom_client"] : (function () { throw new RuntimeError('Variable "nom_client" does not exist.', 10, $this->source); })()), "html", null, true);
+        echo "
+\t\t\t!</h1>
 \t\t<hr class=\"w-75 mx-auto mt-0 mb-3\">
 \t\t<div class=\"mt-3 mb-5 p-3 justify-content-around rounded mnb row\">
 \t\t\t<h3 class=\"d-flex justify-content-center mt-3\">Récapitulatif de votre commande :</h3>
 \t\t\t";
-        // line 13
+        // line 15
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["panier"]) || array_key_exists("panier", $context) ? $context["panier"] : (function () { throw new RuntimeError('Variable "panier" does not exist.', 13, $this->source); })()));
+        $context['_seq'] = twig_ensure_traversable((isset($context["panier"]) || array_key_exists("panier", $context) ? $context["panier"] : (function () { throw new RuntimeError('Variable "panier" does not exist.', 15, $this->source); })()));
         foreach ($context['_seq'] as $context["_key"] => $context["plat"]) {
-            // line 14
+            // line 16
             echo "\t\t\t\t<div class=\"col-md-4 mt-3\">
 \t\t\t\t\t<div class=\"card border-0 shd text-white\">
 \t\t\t\t\t\t<div class=\"row g-0\">
 \t\t\t\t\t\t\t<div class=\"col-md-7 imgof\">
 \t\t\t\t\t\t\t\t<img src=\"";
-            // line 18
-            echo twig_escape_filter($this->env, ((isset($context["image_dir"]) || array_key_exists("image_dir", $context) ? $context["image_dir"] : (function () { throw new RuntimeError('Variable "image_dir" does not exist.', 18, $this->source); })()) . twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 18), "image", [], "any", false, false, false, 18)), "html", null, true);
+            // line 20
+            echo twig_escape_filter($this->env, ((isset($context["image_dir"]) || array_key_exists("image_dir", $context) ? $context["image_dir"] : (function () { throw new RuntimeError('Variable "image_dir" does not exist.', 20, $this->source); })()) . twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 20), "image", [], "any", false, false, false, 20)), "html", null, true);
             echo "\" class=\"img-fluid rounded-start\" alt=\"";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 18), "image", [], "any", false, false, false, 18), "html", null, true);
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 20), "image", [], "any", false, false, false, 20), "html", null, true);
             echo "\">
 \t\t\t\t\t\t\t</div>
 \t\t\t\t\t\t\t<div class=\"card-body col-md-5 d-flex flex-column justify-content-center\">
 \t\t\t\t\t\t\t\t<h5 class=\"card-title text-center\">";
-            // line 21
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 21), "libelle", [], "any", false, false, false, 21), "html", null, true);
+            // line 23
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 23), "libelle", [], "any", false, false, false, 23), "html", null, true);
             echo "</h5>
 \t\t\t\t\t\t\t\t<hr class=\"w-50 mx-auto mt-3 mb-3\">
 \t\t\t\t\t\t\t\t<p class=\"card-text text-center\">";
-            // line 23
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 23), "prix", [], "any", false, false, false, 23), "html", null, true);
+            // line 25
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, $context["plat"], "Plat", [], "array", false, false, false, 25), "prix", [], "any", false, false, false, 25), "html", null, true);
             echo "€</p>
 \t\t\t\t\t\t\t\t<hr class=\"w-50 mx-auto mt-0 mb-3\">
 \t\t\t\t\t\t\t\t<p class=\"card-text text-center\">Quantité:
 \t\t\t\t\t\t\t\t\t";
-            // line 26
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["plat"], "quantite", [], "any", false, false, false, 26), "html", null, true);
+            // line 28
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["plat"], "quantite", [], "any", false, false, false, 28), "html", null, true);
             echo "</p>
 \t\t\t\t\t\t\t</div>
 \t\t\t\t\t\t</div>
@@ -134,17 +139,30 @@ class __TwigTemplate_49c6cb55856af072d44c006a74fd2962 extends Template
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['plat'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 32
-        echo "            <hr class=\"w-100 mx-auto mt-4 mb-3 d-flex\">
-\t\t\t<div class=\"text-center\">Total de votre commande:
-\t\t\t\t";
         // line 34
-        echo twig_escape_filter($this->env, (isset($context["totalprix"]) || array_key_exists("totalprix", $context) ? $context["totalprix"] : (function () { throw new RuntimeError('Variable "totalprix" does not exist.', 34, $this->source); })()), "html", null, true);
-        echo "€</div>
+        echo "\t\t\t<hr class=\"w-100 mx-auto mt-4 mb-3 d-flex\">
+\t\t\t<div class=\"text-center mb-3\">Total de votre commande:<br>
+\t\t\t\t<strong>";
+        // line 36
+        echo twig_escape_filter($this->env, (isset($context["totalprix"]) || array_key_exists("totalprix", $context) ? $context["totalprix"] : (function () { throw new RuntimeError('Variable "totalprix" does not exist.', 36, $this->source); })()), "html", null, true);
+        echo "€</strong>
+\t\t\t</div>
+\t\t\t<div class=\"text-center mb-3\">Votre adresse de livraison:<br>
+\t\t\t\t<strong>";
+        // line 39
+        echo twig_escape_filter($this->env, (isset($context["adresse_client"]) || array_key_exists("adresse_client", $context) ? $context["adresse_client"] : (function () { throw new RuntimeError('Variable "adresse_client" does not exist.', 39, $this->source); })()), "html", null, true);
+        echo "</strong>
+\t\t\t</div>
+\t\t\t<div class=\"text-center\">Un mail de confirmation de votre commande vous à été envoyé à l'adresse suivante :<br>
+\t\t\t\t<strong>";
+        // line 42
+        echo twig_escape_filter($this->env, (isset($context["mail_client"]) || array_key_exists("mail_client", $context) ? $context["mail_client"] : (function () { throw new RuntimeError('Variable "mail_client" does not exist.', 42, $this->source); })()), "html", null, true);
+        echo "</strong>
+\t\t\t</div>
 \t\t\t<hr class=\"w-100 mx-auto mt-3 mb-3 d-flex\">
 \t\t\t<div class=\"text-center\">
 \t\t\t\t<a href=\"";
-        // line 37
+        // line 46
         echo $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("app_accueil");
         echo "\" class=\"btn btn-light mt-4\">Retour accueil</a>
 \t\t\t</div>
@@ -172,7 +190,7 @@ class __TwigTemplate_49c6cb55856af072d44c006a74fd2962 extends Template
 
     public function getDebugInfo()
     {
-        return array (  148 => 37,  142 => 34,  138 => 32,  126 => 26,  120 => 23,  115 => 21,  107 => 18,  101 => 14,  97 => 13,  89 => 7,  79 => 6,  59 => 3,  36 => 1,);
+        return array (  166 => 46,  159 => 42,  153 => 39,  147 => 36,  143 => 34,  131 => 28,  125 => 25,  120 => 23,  112 => 20,  106 => 16,  102 => 15,  94 => 10,  89 => 7,  79 => 6,  59 => 3,  36 => 1,);
     }
 
     public function getSourceContext()
@@ -185,7 +203,9 @@ class __TwigTemplate_49c6cb55856af072d44c006a74fd2962 extends Template
 {% block body %}
 
 \t<main>
-\t\t<h1 class=\"d-flex justify-content-center mt-2\">Merci de votre commande !</h1>
+\t\t<h1 class=\"d-flex justify-content-center mt-2\">Merci de votre commande
+\t\t\t{{ nom_client }}
+\t\t\t!</h1>
 \t\t<hr class=\"w-75 mx-auto mt-0 mb-3\">
 \t\t<div class=\"mt-3 mb-5 p-3 justify-content-around rounded mnb row\">
 \t\t\t<h3 class=\"d-flex justify-content-center mt-3\">Récapitulatif de votre commande :</h3>
@@ -208,9 +228,16 @@ class __TwigTemplate_49c6cb55856af072d44c006a74fd2962 extends Template
 \t\t\t\t\t</div>
 \t\t\t\t</div>
 \t\t\t{% endfor %}
-            <hr class=\"w-100 mx-auto mt-4 mb-3 d-flex\">
-\t\t\t<div class=\"text-center\">Total de votre commande:
-\t\t\t\t{{ totalprix }}€</div>
+\t\t\t<hr class=\"w-100 mx-auto mt-4 mb-3 d-flex\">
+\t\t\t<div class=\"text-center mb-3\">Total de votre commande:<br>
+\t\t\t\t<strong>{{ totalprix }}€</strong>
+\t\t\t</div>
+\t\t\t<div class=\"text-center mb-3\">Votre adresse de livraison:<br>
+\t\t\t\t<strong>{{ adresse_client }}</strong>
+\t\t\t</div>
+\t\t\t<div class=\"text-center\">Un mail de confirmation de votre commande vous à été envoyé à l'adresse suivante :<br>
+\t\t\t\t<strong>{{ mail_client }}</strong>
+\t\t\t</div>
 \t\t\t<hr class=\"w-100 mx-auto mt-3 mb-3 d-flex\">
 \t\t\t<div class=\"text-center\">
 \t\t\t\t<a href=\"{{ path('app_accueil') }}\" class=\"btn btn-light mt-4\">Retour accueil</a>
